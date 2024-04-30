a66b541c80bfd43786b1fddf3846e51fc0f531ae
Lucas A <game4allyt@gmail.com>
2024-04-30 21:23:51 +0200

    Whoops

--- src/root.zig
+++ src/root.zig
@@ -1,15 +1,11 @@
-const perceptron = @import("perceptron.zig");
-
 const std = @import("std");
 pub const math = @import("math.zig");
 pub const activation = @import("nn/activation.zig");
 
-pub const Perceptron = perceptron.Perceptron;
 pub const DenseLayer = @import("nn/dense.zig").DenseLayer;
 pub const Network = @import("nn/network.zig").Network;
 
 comptime {
-    std.testing.refAllDeclsRecursive(perceptron);
     std.testing.refAllDeclsRecursive(@import("nn/dense.zig"));
     std.testing.refAllDeclsRecursive(@import("nn/network.zig"));
     std.testing.refAllDecls(math);
