809d43ba756a089e2e352fb09ab1679747c64317
Lucas A. <game4allyt@gmail.com>
2024-05-08 11:19:07 +0200

    Introduce epsilon in loss inputs to ensure numerical stability

--- src/nn/loss.zig
+++ src/nn/loss.zig
@@ -1,5 +1,9 @@
 const std = @import("std");
 
+// the minimum value that a f32 can handle
+// this gets added to the input value in loss functions which aren't numerically stable (namely log()) to prevent NaNs.
+const F32_MIN_VALUE = std.math.floatMin(f32);
+
 /// A usable loss function.
 pub const Loss = struct {
     compute: fn (f32, f32) f32,
@@ -28,11 +32,11 @@ pub const BinaryCrossEntropy: Loss = .{
 };
 
 fn bce_loss(input: f32, target: f32) f32 {
-    return -((target * @log(input)) + (1.0 - target) * @log(1.0 - input));
+    return -((target * @log(input + F32_MIN_VALUE)) + (1.0 - target) * @log(1.0 - input + F32_MIN_VALUE));
 }
 
 fn bce_derivative(input: f32, target: f32) f32 {
-    return -((input - target) / (input * (1.0 - input)));
+    return -((input + F32_MIN_VALUE - target) / ((input + F32_MIN_VALUE) * (1.0 - input + F32_MIN_VALUE)));
 }
 
 /// Categorical cross entropy.
@@ -43,7 +47,7 @@ pub const CategoricalCrossEntropy: Loss = .{
 };
 
 fn cce_loss(input: f32, target: f32) f32 {
-    return -target * @log(input);
+    return -target * @log(input + F32_MIN_VALUE);
 }
 
 fn cce_derivative(input: f32, target: f32) f32 {
