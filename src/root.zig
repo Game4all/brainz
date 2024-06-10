765aa0b47b4d62513494f07d979d8e28cf86cd13
Lucas A. <game4allyt@gmail.com>
2024-06-10 11:49:56 +0200

    Add a matrix types with most basic associated operations

--- src/root.zig
+++ src/root.zig
@@ -1,14 +1,19 @@
 const std = @import("std");
-pub const math = @import("math.zig");
+
 pub const activation = @import("nn/activation.zig");
 pub const loss = @import("nn/loss.zig");
 pub const meta = @import("nn/meta.zig");
+pub const matrix = struct {
+    usingnamespace @import("matrix.zig");
+    usingnamespace @import("matrix_ops.zig");
+};
 
+pub const Matrix = matrix.Matrix;
 pub const DenseLayer = @import("nn/dense.zig").DenseLayer;
 pub const Network = @import("nn/network.zig").Network;
 
 comptime {
     std.testing.refAllDeclsRecursive(@import("nn/dense.zig"));
     std.testing.refAllDeclsRecursive(@import("nn/network.zig"));
-    std.testing.refAllDecls(math);
+    std.testing.refAllDeclsRecursive(matrix);
 }
