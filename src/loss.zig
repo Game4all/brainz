ac079d9c4f04b77c2eead8fccda02037f24f97db
Lucas A. <game4allyt@gmail.com>
2024-06-12 17:10:55 +0200

    Rewrite loss functions to use matrices

--- src/loss.zig
+++ src/loss.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const ops = @import("ops.zig");
+const Matrix = @import("matrix.zig").Matrix;
 
 // the minimum value that a f32 can handle
 // this gets added to the input value in loss functions which aren't numerically stable (namely log()) to prevent NaNs.
@@ -6,8 +8,8 @@ const F32_MIN_VALUE = std.math.floatMin(f32);
 
 /// A usable loss function.
 pub const Loss = struct {
-    compute: *const fn (f32, f32) f32,
-    compute_derivative: *const fn (f32, f32) f32,
+    compute: *const fn (*const Matrix(f32), *const Matrix(f32)) f32,
+    compute_derivative: *const fn (*const Matrix(f32), *const Matrix(f32), *Matrix(f32)) void,
 };
 
 /// Mean Squared Error or L2 loss.
@@ -16,12 +18,19 @@ pub const MSE: Loss = .{
     .compute_derivative = mse_derivative,
 };
 
-fn mse_loss(input: f32, target: f32) f32 {
-    return (1.0 / 2.0) * std.math.pow(f32, target - input, 2.0);
+fn mse_loss(input: *const Matrix(f32), target: *const Matrix(f32)) f32 {
+    var loss: f32 = 0.0;
+    const len = input.get_slice().len;
+
+    for (input.get_slice(), target.get_slice()) |i, t|
+        loss += (1.0 / @as(f32, @floatFromInt(len))) * std.math.pow(f32, t - i, 2.0);
+
+    return loss;
 }
 
-fn mse_derivative(input: f32, target: f32) f32 {
-    return (target - input);
+fn mse_derivative(in: *const Matrix(f32), target: *const Matrix(f32), result: *Matrix(f32)) void {
+    ops.sub(f32, target, in, result);
+    return result;
 }
 
 /// Binary cross entropy.
@@ -31,12 +40,21 @@ pub const BinaryCrossEntropy: Loss = .{
     .compute_derivative = bce_derivative,
 };
 
-fn bce_loss(input: f32, target: f32) f32 {
-    return -((target * @log(input + F32_MIN_VALUE)) + (1.0 - target) * @log(1.0 - input + F32_MIN_VALUE));
+fn bce_loss(input: *const Matrix(f32), target: *const Matrix(f32)) f32 {
+    var loss: f32 = 0.0;
+    const len = input.get_slice().len;
+
+    for (input.get_slice(), target.get_slice()) |i, t|
+        loss += -(1.0 / @as(f32, @floatFromInt(len))) * ((t * @log(i + F32_MIN_VALUE)) + (1.0 - t) * @log(1.0 - i + F32_MIN_VALUE));
+
+    return loss;
 }
 
-fn bce_derivative(input: f32, target: f32) f32 {
-    return -((input + F32_MIN_VALUE - target) / ((input + F32_MIN_VALUE) * (1.0 - input + F32_MIN_VALUE)));
+fn bce_derivative(input: *const Matrix(f32), target: *const Matrix(f32), result: *Matrix(f32)) void {
+    for (input.get_slice(), target.get_slice(), result.get_mut_slice()) |i, t, *r|
+        r.* = -((i + F32_MIN_VALUE - t) / ((i + F32_MIN_VALUE) * (1.0 - i + F32_MIN_VALUE)));
+
+    return result;
 }
 
 /// Categorical cross entropy.
@@ -46,10 +64,17 @@ pub const CategoricalCrossEntropy: Loss = .{
     .compute_derivative = bce_derivative,
 };
 
-fn cce_loss(input: f32, target: f32) f32 {
-    return -target * @log(input + F32_MIN_VALUE);
+fn cce_loss(input: *const Matrix(f32), target: *const Matrix(f32)) f32 {
+    var loss: f32 = 0.0;
+    const len = input.get_slice().len;
+
+    for (input.get_slice(), target.get_slice()) |i, t|
+        loss += -(1.0 / @as(f32, @floatFromInt(len))) * t * @log(i + F32_MIN_VALUE);
+
+    return loss;
 }
 
-fn cce_derivative(input: f32, target: f32) f32 {
-    return input - target;
+fn cce_derivative(in: *const Matrix(f32), target: *const Matrix(f32), result: *Matrix(f32)) void {
+    ops.sub(f32, in, target, result);
+    return result;
 }
