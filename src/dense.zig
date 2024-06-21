e05125dfc2bda5b075c12fd56e6ea7b470a9d86c
Lucas A. <game4allyt@gmail.com>
2024-06-21 12:04:26 +0200

    Reenable tests

--- src/dense.zig
+++ src/dense.zig
@@ -47,9 +47,9 @@ pub fn Dense(comptime num_in: usize, comptime num_out: usize, comptime activatio
             std.debug.assert(w.len == num_in * num_out);
             std.debug.assert(b.len == num_out);
 
-            self.weights = try Tensor(f32).empty(.{ num_out, num_in }, alloc);
-            self.biases = try Tensor(f32).empty(.{ num_out, 1 }, alloc);
-            self.last_outputs = try Tensor(f32).empty(try root.ops.opShape(.MatMul, self.weights.shape, .{ num_in, 1 }), alloc);
+            self.weights = try Tensor(f32).empty(.{ 0, num_out, num_in }, alloc);
+            self.biases = try Tensor(f32).empty(.{ 0, num_out, 1 }, alloc);
+            self.last_outputs = try Tensor(f32).empty(try root.ops.opShape(.MatMul, self.weights.shape, .{ 0, num_in, 1 }), alloc);
             self.activation_outputs = try Tensor(f32).empty(self.last_outputs.shape, alloc);
 
             self.grad = try Tensor(f32).empty(self.last_outputs.shape, alloc);
@@ -98,136 +98,136 @@ pub fn Dense(comptime num_in: usize, comptime num_out: usize, comptime activatio
     };
 }
 
-// test "basic xor mlp" {
-//     const activations = @import("activation.zig");
+test "basic xor mlp" {
+    const activations = @import("activation.zig");
 
-//     var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
-//     defer arena.deinit();
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
 
-//     const alloc = arena.allocator();
+    const alloc = arena.allocator();
 
-//     const XorMLP = struct {
-//         layer_1: Dense(2, 2, activations.Heaviside) = undefined,
-//         layer_2: Dense(2, 1, activations.Heaviside) = undefined,
+    const XorMLP = struct {
+        layer_1: Dense(2, 2, activations.Heaviside) = undefined,
+        layer_2: Dense(2, 1, activations.Heaviside) = undefined,
 
-//         pub fn forward(self: *@This(), input: *Matrix(f32)) *Matrix(f32) {
-//             const a = self.layer_1.forward(input);
-//             return self.layer_2.forward(a);
-//         }
+        pub fn forward(self: *@This(), input: *Tensor(f32)) *Tensor(f32) {
+            const a = self.layer_1.forward(input);
+            return self.layer_2.forward(a);
+        }
 
-//         pub fn init(self: *@This(), allocator: Allocator) !void {
-//             try self.layer_1.initWithWeights(
-//                 allocator,
-//                 @constCast(&[_]f32{ 1.0, 1.0, 1.0, 1.0 }),
-//                 @constCast(&[_]f32{ -0.5, -1.5 }),
-//             );
-//             try self.layer_2.initWithWeights(
-//                 allocator,
-//                 @constCast(&[_]f32{ 1.0, -1.0 }),
-//                 @constCast(&[_]f32{0.0}),
-//             );
-//         }
+        pub fn init(self: *@This(), allocator: Allocator) !void {
+            try self.layer_1.initWithWeights(
+                allocator,
+                @constCast(&[_]f32{ 1.0, 1.0, 1.0, 1.0 }),
+                @constCast(&[_]f32{ -0.5, -1.5 }),
+            );
+            try self.layer_2.initWithWeights(
+                allocator,
+                @constCast(&[_]f32{ 1.0, -1.0 }),
+                @constCast(&[_]f32{0.0}),
+            );
+        }
 
-//         pub fn deinit(self: *@This()) void {
-//             self.layer_1.deinit();
-//             self.layer_2.deinit();
-//         }
-//     };
+        pub fn deinit(self: *@This()) void {
+            self.layer_1.deinit();
+            self.layer_2.deinit();
+        }
+    };
 
-//     var xor_mlp: XorMLP = undefined;
-//     try xor_mlp.init(alloc);
+    var xor_mlp: XorMLP = undefined;
+    try xor_mlp.init(alloc);
 
-//     var inputs = try Matrix(f32).empty(.{ 2, 1 }, alloc);
+    var inputs = try Tensor(f32).empty(.{ 0, 2, 1 }, alloc);
 
-//     const ins = [_][2]f32{
-//         [2]f32{ 0.0, 0.0 },
-//         [2]f32{ 0.0, 1.0 },
-//         [2]f32{ 1.0, 0.0 },
-//         [2]f32{ 1.0, 1.0 },
-//     };
+    const ins = [_][2]f32{
+        [2]f32{ 0.0, 0.0 },
+        [2]f32{ 0.0, 1.0 },
+        [2]f32{ 1.0, 0.0 },
+        [2]f32{ 1.0, 1.0 },
+    };
 
-//     const outputs = [_][1]f32{
-//         [1]f32{0.0},
-//         [1]f32{1.0},
-//         [1]f32{1.0},
-//         [1]f32{0.0},
-//     };
+    const outputs = [_][1]f32{
+        [1]f32{0.0},
+        [1]f32{1.0},
+        [1]f32{1.0},
+        [1]f32{0.0},
+    };
 
-//     for (ins, outputs) |in, outs| {
-//         @memcpy(inputs.slice(), @constCast(&in));
-//         const prediction = xor_mlp.forward(&inputs);
-//         try std.testing.expectEqualSlices(f32, &outs, prediction.constSlice());
-//     }
-// }
+    for (ins, outputs) |in, outs| {
+        @memcpy(inputs.slice(), @constCast(&in));
+        const prediction = xor_mlp.forward(&inputs);
+        try std.testing.expectEqualSlices(f32, &outs, prediction.constSlice());
+    }
+}
 
-// test "linear regression backprop test" {
-//     const Linear = @import("activation.zig").Linear;
-//     const MSE = @import("loss.zig").MSE;
-//     const ops = @import("ops.zig");
+test "linear regression backprop test" {
+    const Linear = @import("activation.zig").Linear;
+    const MSE = @import("loss.zig").MSE;
+    const ops = @import("ops.zig");
 
-//     var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
-//     defer arena.deinit();
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
 
-//     const alloc = arena.allocator();
-//     const inputs = [_][1]f32{
-//         [1]f32{0.0},
-//         [1]f32{1.0},
-//         [1]f32{3.0},
-//         [1]f32{9.0},
-//     };
+    const alloc = arena.allocator();
+    const inputs = [_][1]f32{
+        [1]f32{0.0},
+        [1]f32{1.0},
+        [1]f32{3.0},
+        [1]f32{9.0},
+    };
 
-//     // outputs follow f(x)=2x + 1
-//     const outputs = [_][1]f32{
-//         [1]f32{1.0},
-//         [1]f32{3.0},
-//         [1]f32{7.0},
-//         [1]f32{19.0},
-//     };
+    // outputs follow f(x)=2x + 1
+    const outputs = [_][1]f32{
+        [1]f32{1.0},
+        [1]f32{3.0},
+        [1]f32{7.0},
+        [1]f32{19.0},
+    };
 
-//     var regressor: Dense(1, 1, Linear) = undefined;
-//     try regressor.init(alloc);
+    var regressor: Dense(1, 1, Linear) = undefined;
+    try regressor.init(alloc);
 
-//     // contains the expected value for backprop
-//     var expected_mat = try Matrix(f32).empty(.{ 1, 1 }, alloc);
-//     // contains the computed loss gradient
-//     var loss_grad = try Matrix(f32).empty(.{ 1, 1 }, alloc);
+    // contains the expected value for backprop
+    var expected_mat = try Tensor(f32).empty(.{ 0, 1, 1 }, alloc);
+    // contains the computed loss gradient
+    var loss_grad = try Tensor(f32).empty(.{ 0, 1, 1 }, alloc);
 
-//     // contains the input of the network
-//     var input_mat = try Matrix(f32).empty(.{ 1, 1 }, alloc);
-//     var input_transposed = input_mat.transpose();
+    // contains the input of the network
+    var input_mat = try Tensor(f32).empty(.{ 0, 1, 1 }, alloc);
+    var input_transposed = input_mat.transpose();
 
-//     // contains the gradient wrt to the weights
-//     var weights_grad = try Matrix(f32).empty(.{ 1, 1 }, alloc);
+    // contains the gradient wrt to the weights
+    var weights_grad = try Tensor(f32).empty(.{ 0, 1, 1 }, alloc);
 
-//     // train for 100 epochs.
-//     for (0..100) |_| {
-//         for (inputs, outputs) |i, o| {
-//             @memcpy(input_mat.slice(), @constCast(&i));
-//             @memcpy(expected_mat.slice(), @constCast(&o));
+    // train for 100 epochs.
+    for (0..100) |_| {
+        for (inputs, outputs) |i, o| {
+            @memcpy(input_mat.slice(), @constCast(&i));
+            @memcpy(expected_mat.slice(), @constCast(&o));
 
-//             const result = regressor.forward(&input_mat);
-//             MSE.computeDerivative(result, &expected_mat, &loss_grad);
+            const result = regressor.forward(&input_mat);
+            MSE.computeDerivative(result, &expected_mat, &loss_grad);
 
-//             // compute the gradients for the layer.
-//             // they are stored in the `.grad`
-//             _ = regressor.backwards(&loss_grad);
-//             ops.mulScalar(f32, &regressor.grad, 0.1, &regressor.grad); // scale the error gradient by 0.1 so we don't have to do it twice for the weight and bias update.
+            // compute the gradients for the layer.
+            // they are stored in the `.grad`
+            _ = regressor.backwards(&loss_grad);
+            ops.mulScalar(f32, &regressor.grad, 0.1, &regressor.grad); // scale the error gradient by 0.1 so we don't have to do it twice for the weight and bias update.
 
-//             // compute the grad wrt to the weights.
-//             ops.matMul(f32, &regressor.grad, &input_transposed, &weights_grad);
+            // compute the grad wrt to the weights.
+            ops.matMul(f32, &regressor.grad, &input_transposed, &weights_grad);
 
-//             // update the weights
-//             ops.sub(f32, &regressor.weights, &weights_grad, &regressor.weights); // Wnew = Wold - Wgrad;
-//             // update the bias
-//             ops.sub(f32, &regressor.biases, &regressor.grad, &regressor.biases); // Bnew = Bold - grad;
-//         }
-//     }
+            // update the weights
+            ops.sub(f32, &regressor.weights, &weights_grad, &regressor.weights); // Wnew = Wold - Wgrad;
+            // update the bias
+            ops.sub(f32, &regressor.biases, &regressor.grad, &regressor.biases); // Bnew = Bold - grad;
+        }
+    }
 
-//     for (inputs, outputs) |i, o| {
-//         @memcpy(input_mat.slice(), @constCast(&i));
-//         @memcpy(expected_mat.slice(), @constCast(&o));
+    for (inputs, outputs) |i, o| {
+        @memcpy(input_mat.slice(), @constCast(&i));
+        @memcpy(expected_mat.slice(), @constCast(&o));
 
-//         const result = regressor.forward(&input_mat);
-//         try std.testing.expectApproxEqAbs(o[0], result.get(.{ 0, 0 }), 1.0e-5);
-//     }
-// }
+        const result = regressor.forward(&input_mat);
+        try std.testing.expectApproxEqAbs(o[0], result.get(.{ 0, 0, 0 }), 1.0e-5);
+    }
+}
