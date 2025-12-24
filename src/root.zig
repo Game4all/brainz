1835a71adb6807eb9d9976dddd4b4231507ec3e6
Lucas A. <game4allyt@gmail.com>
2025-12-24 18:16:31 +0100

    Add type defs to root

--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,11 @@
 const std = @import("std");
 const tensor = @import("tensor.zig");
 
+const Tensor = tensor.Tensor;
+const TensorArena = tensor.TensorArena;
+const Dtype = tensor.Dtype;
+const Shape = tensor.Shape;
+
 comptime {
     std.testing.refAllDeclsRecursive(tensor);
 }
