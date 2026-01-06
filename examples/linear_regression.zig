074564ad1cd12bf888dd87af24240bc59b9abc9e
Lucas A. <game4allyt@gmail.com>
2026-01-06 12:56:16 +0100

    Update linear regression example to use batching

--- examples/linear_regression.zig
+++ examples/linear_regression.zig
@@ -1,4 +1,4 @@
-///! Linear regression example where a weight tensor is optimized with SGD to predict points following y = 2x + 1 equation curve
+///! Linear regression example where a weight and a bias are optimized with SGD to predict points following y = 2x + 1 equation curve
 const std = @import("std");
 const brainz = @import("brainz");
 
@@ -20,28 +20,32 @@ pub fn main() !void {
     var tensorArena: TensorArena = .init(allocator);
     defer tensorArena.deinit();
 
-    const N: usize = 100; // number of samples
+    const N: usize = 2500; // number of samples
     const lr: f32 = 0.01;
     const epochs: usize = 2000;
 
-    // we use augmented matrices to represent the input x with the bias term
-    const x_shape: Shape = comptime .fromSlice(&.{ N, 2 });
+    const x_shape: Shape = comptime .fromSlice(&.{ N, 1 });
     const y_shape: Shape = comptime .fromSlice(&.{ N, 1 });
-    const w_shape: Shape = comptime .fromSlice(&.{ 2, 1 }); // weight tensor shape
+
+    // we use separate tensors for weight and bias, which are basically scalars in that case
+    const w_shape: Shape = comptime .fromSlice(&.{1});
+    const b_shape: Shape = comptime .fromSlice(&.{1});
 
     // define plan and linear regression model
     var plan: ExecutionPlan = .init(&tensorArena, allocator);
     defer plan.deinit();
 
-    // create X and target Y inputs (which both are not trainable hence do not require gradients)
+    // create X and target Y inputs
     const x = try plan.createInput("x", .float32, x_shape, false);
     const y_target = try plan.createInput("y", .float32, y_shape, false);
 
-    // create a tensor for model parameters (which are trainable)
+    // create tensors for model parameters (trainable)
     const w = try plan.createParam(.float32, w_shape);
+    const b = try plan.createParam(.float32, b_shape);
 
-    // y_pred = x @ w which is a dot product
-    const y_pred = try ops.matmul(&plan, x, w);
+    // y_pred = x * w + b
+    const xw = try ops.mul(&plan, x, w);
+    const y_pred = try ops.add(&plan, xw, b);
 
     // loss = mse(y_pred, y_target)
     const loss = try ops.mseLoss(&plan, y_pred, y_target);
@@ -61,24 +65,27 @@ pub fn main() !void {
 
     for (0..N) |i| {
         const val = random.float(f32) * 10.0;
-        x_data[i * 2] = val; // x feature
-        x_data[i * 2 + 1] = 1.0; // bias term
+        x_data[i] = val; // x feature
 
         // y = 2x + 1 + noise
         const noise = (random.float(f32) - 0.5) * 0.5;
         y_data[i] = 2.0 * val + 1.0 + noise;
     }
 
-    // initialize weights randomly
+    // initialize weights and bias randomly
     const w_data = w.slice(f32).?;
     for (w_data) |*w_i|
         w_i.* = random.floatNorm(f32);
+
+    const b_data = b.slice(f32).?;
+    for (b_data) |*b_i|
+        b_i.* = random.floatNorm(f32);
 
     // compute initial MSE for display
     try plan.forward();
     const initialLoss = loss.scalar(f32).?;
 
-    std.log.info("Initial weights: w={d:.4}, b={d:.4}, MSE={d:.4}", .{ w_data[0], w_data[1], initialLoss });
+    std.log.info("Initial weights: w={d:.4}, b={d:.4}, MSE={d:.4}", .{ w_data[0], b_data[0], initialLoss });
 
     var sgd = optim.SGD.init(plan.getParams(), lr);
     const loss_grad = loss.grad.?.slice(f32).?;
@@ -87,7 +94,8 @@ pub fn main() !void {
         try plan.forward();
 
         const current_loss = loss.scalar(f32).?;
-        std.log.info("Epoch {}: Loss = {d:.6}", .{ epoch, current_loss });
+        if (epoch % 100 == 0)
+            std.log.info("Epoch {}: Loss = {d:.6}", .{ epoch, current_loss });
 
         // zero out the gradients
         plan.zeroGrad();
@@ -102,5 +110,5 @@ pub fn main() !void {
     const finalLoss = loss.scalar(f32).?;
 
     // Result
-    std.log.info("Final weights: w={d:.4}, b={d:.4}, Final MSE={d:.4}", .{ w_data[0], w_data[1], finalLoss });
+    std.log.info("Final weights: w={d:.4}, b={d:.4}, Final MSE={d:.4}", .{ w_data[0], b_data[0], finalLoss });
 }
